b97bdcab13c342a8d817983591a15630952c128e
emacslisp <java.gdb@gmail.com>
2017-07-20 11:12:41 +1000

    vector test

--- STLMain/VecorTest.cpp
+++ STLMain/VecorTest.cpp
@@ -6,9 +6,22 @@
  */
 
 #include<iostream>
+#include<vector>
+
 using namespace std;
 int main()
 {
-	cout<<"test eclipse"<<endl;
-	return 0;
+	  int myints[]={10,20,30,40,50,60,70};
+	  vector<int> myvector (7);
+
+	  copy ( myints, myints+7, myvector.begin() );
+
+	  cout << "myvector contains:";
+	  for (std::vector<int>::iterator it = myvector.begin(); it!=myvector.end(); ++it)
+	    std::cout << ' ' << *it;
+
+	  std::cout << '\n';
+
+	  return 0;
+
 }
