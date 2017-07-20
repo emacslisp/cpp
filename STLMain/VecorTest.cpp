6793746c126394411288092d0c16106642eed13d
emacslisp <java.gdb@gmail.com>
2017-07-20 11:00:53 +1000

    eclipse cpp test

---
+++ STLMain/VecorTest.cpp
@@ -0,0 +1,14 @@
+/*
+ * VecorTest.cpp
+ *
+ *  Created on: Jul 20, 2017
+ *      Author: ewu
+ */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"test eclipse"<<endl;
+	return 0;
+}
