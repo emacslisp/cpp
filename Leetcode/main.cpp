a6c7692267affccc18974dc93dc1ca90fa013a5f
emacslisp <java.gdb@gmail.com>
2017-07-21 10:50:36 +1000

    add Leetcode for c++ coding

---
+++ Leetcode/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 21, 2017
+ *      Author: ewu
+ */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Leetcode Entry"<<endl;
+	return 0;
+}
