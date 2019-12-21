fa1b4eb82af8892e0b80c064e139bf01d17dedd8
emacslisp <java.gdb@gmail.com>
2019-12-21 16:07:04 +1100

    #add cpp-cli for xcode

---
+++ cpp-cli/cpp-cli/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  cpp-cli
+//
+//  Created by ewu on 21/8/19.
+//  Copyright © 2019 ewu. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello from XCode" <<endl;
+    return 0;
+}
