c3e5dbc1c1c846350304f79aaa1bebb85b20f8e2
emacslisp <java.gdb@gmail.com>
2019-12-27 23:51:27 +1100

    add mktime and vsprintf

---
+++ linux-0.11-cli/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: 27 Dec 2019
+ *      Author: wudi
+ */
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+int main(int argc, char *argv[]) {
+
+	// printf("linux 0.11 cli");
+	mktime_test();
+	return 0;
+}
