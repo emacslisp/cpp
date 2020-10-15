2ac01914e9405be4aeb1484fd81a65492dee15dc
emacslisp <java.gdb@gmail.com>
2020-10-16 04:36:30 +1100

    add map test for cpp vector and gitignore

--- cpp-cli/cpp-cli/MapTest.cpp
+++ cpp-cli/cpp-cli/MapTest.cpp
@@ -9,5 +9,26 @@
 #include "MapTest.hpp"
 
 void MapTest::main() {
-    cout<<"Map Test"<<endl;
+    map<string,int> ages;
+    
+    ages["Mike"] = 40;
+    ages["John"] = 50;
+    ages["Vicky"] = 20;
+    
+    cout << ages["Mike"] << endl;
+    
+    ages.insert(pair<string, int> ("Peter", 100));
+    
+    for(map<string, int>::iterator it = ages.begin(); it!=ages.end();it++) {
+        cout << it->first << ": " << it->second<<endl;
+    }
+    
+    cout << "============" <<endl;
+    
+    for(map<string, int>::iterator it = ages.begin(); it!=ages.end();it++) {
+        pair<string, int> age = *it;
+        cout << age.first << ": " << age.second<<endl;
+    }
+    
+    cout << "============" <<endl;
 }
