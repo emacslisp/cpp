89fb733404855d9c17eb842635cd9973af30d5ef
emacslisp <java.gdb@gmail.com>
2021-01-03 09:57:43 +1100

    finish poj 1014

---
+++ leetcode-cpp/poj_dividing_1014.cpp
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#define FILEIO
+#define Max(a, b) a > b ? a : b
+
+void output(bool isDivided, int collection)
+{
+    printf("Collection #%d:\n", collection);
+
+    if (isDivided)
+    {
+        printf("Can be divided.\n");
+    }
+    else
+    {
+        printf("Can't be divided.\n");
+    }
+    printf("\n");
+}
+
+bool check(vector<int> nums, int sum)
+{
+    vector<vector<int>> dp(sum + 1, vector<int>(nums.size() + 1, 0));
+
+    for (int i = 1; i <= sum; i++)
+    {
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (nums[j] > sum)
+            {
+                return false;
+            }
+            if (nums[j] <= i)
+            {
+                dp[i][j + 1] = Max(dp[i][j], nums[j] + dp[i - nums[j]][j]);
+            }
+        }
+    }
+
+    if (dp[sum][nums.size()] == sum)
+    {
+        return true;
+    }
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+
+#ifdef FILEIO
+    freopen("poj_dividing_1014.in", "r", stdin);
+    freopen("poj_dividing_1014.out", "w", stdout);
+#endif
+
+    int a[6];
+    bool isEnd;
+    int collection = 0;
+    while (true)
+    {
+        isEnd = true;
+        for (int i = 0; i < 6; i++)
+        {
+            scanf("%d", &a[i]);
+            if (a[i] != 0)
+            {
+                isEnd = false;
+            }
+        }
+
+        if (isEnd)
+        {
+            break;
+        }
+
+        collection++;
+        int sum = 0;
+        vector<int> nums;
+        for (int i = 0; i < 6; i++)
+        {
+            sum += a[i] * (i + 1);
+            for (int j = 0; j < a[i]; j++)
+            {
+                nums.push_back(i + 1);
+            }
+        }
+
+        if (sum % 2 == 1)
+        {
+            output(false, collection);
+            continue;
+        }
+
+        sum /= 2;
+
+        bool result = check(nums, sum);
+
+        output(result, collection);
+    }
+
+#ifdef FILEIO
+    fclose(stdin);
+    fclose(stdout);
+#endif
+    return 0;
+}
