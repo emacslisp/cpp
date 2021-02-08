ee8985b2a212fc49962e092a69a44443f24ed7af
emacslisp <java.gdb@gmail.com>
2021-02-09 07:07:49 +1100

    add daysOfWeek

--- leetcode-cpp/lib/DateTimeHelper.cpp
+++ leetcode-cpp/lib/DateTimeHelper.cpp
@@ -22,10 +22,44 @@ bool DateTimeHelper::isLeapYear(int year)
     return leap;
 }
 
+string DateTimeHelper::daysOfWeek(int day, int month, int year) {
+    vector<int> daysOfMonth {
+        31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    };
+    vector<string> output {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    
+    int totalDays = 0;
+    for(int i=1970;i<year;i++) {
+        if(isLeapYear(i)) {
+            totalDays += 366;
+        } else {
+            totalDays += 365;
+        }
+    }
+
+    for(int i=1;i<month;i++) {
+        totalDays += daysOfMonth[i - 1];
+    }
+
+    if(month > 2 && isLeapYear(year)) {
+        totalDays += 1;
+    }
+
+    totalDays += day;
+
+    // 1970, 1 and 1 is Thursday
+    int offset = (3 + totalDays) % 7;
+    return output[offset];
+}
+
 void DateTimeHelper::MainEntry() {
     int year = 2016;
     DateTimeHelper s;
     bool isLeapYear = s.isLeapYear(year);
     cout << "================= DateTimeHelper =====================" <<endl; 
     cout << isLeapYear << endl;
+
+    int day = 9, month = 2;
+    year = 2021;
+    cout << s.daysOfWeek(9, 2, 2021) << endl;
 }
--- leetcode-cpp/lib/DateTimeHelper.h
+++ leetcode-cpp/lib/DateTimeHelper.h
@@ -6,5 +6,6 @@ using namespace std;
 class DateTimeHelper {
 public:
     bool isLeapYear(int year);
+    string daysOfWeek(int day, int month, int year);
     void MainEntry();
 };
