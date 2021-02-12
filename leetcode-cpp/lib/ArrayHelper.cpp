27125bdc072852ff31900c3da46e10d79a938dfa
emacslisp <java.gdb@gmail.com>
2021-02-13 00:19:08 +1100

    add intersection of two arrays

--- leetcode-cpp/lib/ArrayHelper.cpp
+++ leetcode-cpp/lib/ArrayHelper.cpp
@@ -27,6 +27,24 @@ int ArrayHelper::maxSubArray(vector<int> nums) {
       return maxSum > sum ? maxSum : sum;
 }
 
+vector<int> ArrayHelper::intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        map<int, int> m1;
+        map<int, int> m2;
+        for(int x: nums1) m1[x]++;
+        for(int x: nums2) m2[x]++;
+
+        for(auto x: m1) {
+            if(m2.count(x.first) > 0) {
+                int p = min(x.second, m2[x.first]);
+                for(int i=0;i<p;i++) {
+                    result.push_back(x.first);
+                }
+            }
+        }
+        return result;
+}
+
 void ArrayHelper::MainEntry() {
     vector<int> c = {
         -2,1,-3,4,-1,2,1,-5,4
--- leetcode-cpp/lib/ArrayHelper.h
+++ leetcode-cpp/lib/ArrayHelper.h
@@ -1,11 +1,16 @@
-#include<vector>
-#include<iostream>
+#include <vector>
+#include <iostream>
+#include <climits>
 #include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
 using namespace std;
 
 class ArrayHelper {
 public:
     vector<int> revert(vector<int> c);
     int maxSubArray(vector<int> c);
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2);
     void MainEntry();
 };
