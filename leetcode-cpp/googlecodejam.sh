2797215b295309154731633a8f4caf732e456992
emacslisp <java.gdb@gmail.com>
2021-02-17 07:44:40 +1100

    create new template folder and update sh

--- leetcode-cpp/googlecodejam.sh
+++ leetcode-cpp/googlecodejam.sh
@@ -1,5 +1,5 @@
 cp ParentingPartneringReturns_2020_C.cpp $1.cpp
-cp ParentingPartneringReturns_2020_C.sh $1.sh
+cp ./template/poj_run.sh $1.sh
 replace "/ParentingPartneringReturns_2020_C/$1/i" $1.cpp
 chmod 755 $1.sh
 touch $1.in $1.out
--- leetcode-cpp/poj.sh
+++ leetcode-cpp/poj.sh
@@ -1,5 +1,5 @@
 cp Palindrome_poj1159.cpp $1.cpp
-cp JumpGameVI_5631.sh $1.sh
+cp ./template/poj_run.sh $1.sh
 replace "/Palindrome_poj1159/$1/i" $1.cpp
 chmod 755 $1.sh
 touch $1.in $1.out
