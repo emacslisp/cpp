0964468382a3a1139de8649e01da3ef2994093bd
emacslisp <java.gdb@gmail.com>
2021-03-05 14:33:52 +1100

    finish leetcode 441

---
+++ leetcode-cpp/ArrangingCoins_441.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
+#include <math.h>
+
+#define Max(a, b) a > b ? a : b
+#define Min(a, b) a < b ? a : b
+
+using namespace std;
+
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        #define ll long long
+        ll c = (ll)8*(ll)n + (ll)1;
+        int level = (sqrt(c) - 1)/2;
+        
+        return level;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> c
+    {
+       4,5,6,7,0,2,1,3
+    };
+
+    string str = "codeleet";
+    int n = 1804289383;
+    int result = s.arrangeCoins(n);
+    cout<<result<<endl;
+}
