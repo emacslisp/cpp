df7d957f6ec0f79617999284060824b0750a7a57
emacslisp <java.gdb@gmail.com>
2021-04-10 15:43:33 +1000

    finish A 1A 2021

--- leetcode-cpp/AppendSort_1A_2021.cpp
+++ leetcode-cpp/AppendSort_1A_2021.cpp
@@ -19,7 +19,45 @@ vector<int> numOfDigit(ll n) {
         v.push_back(n%10);
         n/=10;
     }
+    reverse(v.begin(), v.end());
     return v;
+}
+
+int compare(vector<int> v1, vector<int> v2) {
+    if(v1.size() > v2.size()) return 1;
+    if(v1.size() < v2.size()) return -1;
+
+    for(int i=0;i<v1.size();i++) {
+        if(v1[i] > v2[i]) {
+            return 1;
+        } else if(v1[i] < v2[i]) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+vector<int> minute(vector<int> v1, vector<int> v2) {
+    vector<int> result;
+    for(int i=0;i<v1.size();i++) {
+        result.push_back(v1[i]- v2[i]);
+    }
+
+    for(int i=0;i<result.size();) {
+        if(result[i] == 0) {
+            result.erase(result.begin() + i);
+        } else {
+            break;
+        }
+    }
+
+    for(int i=0;i<result.size();i++) {
+        if(result[i] < 0) {
+            result[i]+=10;
+            result[i-1] -= 1;
+        }
+    }
+    return result;
 }
 
 int main(int argc, char **argv)
@@ -41,43 +79,52 @@ int main(int argc, char **argv)
         for(int u = 0; u<N; u++) {
             scanf("%lld", &array[u]);
         }
-        int result = 0;
-        for(int i=0;i<N-1;i++) {
-            if(array[i] < array[i+1]) continue;
+        ll result = 0;
+        vector<vector<int> > v;
+        for(int i=0;i<N;i++) {
+            v.push_back(numOfDigit(array[i]));
+        }
 
-            if(array[i] == array[i+1]) {
-                array[i+1] *= 10;
+        for(int i=0;i<N-1;i++) {
+            if(compare(v[i], v[i+1]) == -1) continue;
+
+            if(compare(v[i],v[i+1]) == 0) {
+                v[i+1].push_back(0);
                 result += 1;
             }
 
-            if(array[i] > array[i+1]) {
-                vector<int> v1 = numOfDigit(array[i]);
-                vector<int> v2 = numOfDigit(array[i+1]);
-
-                int offset = 1;
-                int t = (v1.size() - v2.size());
-                result += t;
-                for(int x=0;x<t;x++) {
-                    v2.push_back(0);
-                    array[i+1] *= 10;
-                    array[i+1] += 9;
-                    offset *= 10;
+            if(compare(v[i],v[i+1]) == 1) {
+                vector<int> v3;
+                vector<int> offset;
+                for(int x: v[i + 1]) {
+                    v3.push_back(x);
                 }
 
-                if(array[i] >= array[i+1]) {
-                    array[i+1] /= offset;
+                ll t = (v[i].size() - v[i+1].size());
+                offset.push_back(1);
+                result += t;
+                for(int x=0;x<t;x++) {
+                    v3.push_back(9);
+                    offset.push_back(0);
+                }
+
+                if(compare(v[i],v3) >= 0) {
                     for(int x=0;x<t + 1;x++) {
-                        array[i+1] *= 10;
+                        v[i+1].push_back(0);
                     }
                     result += 1;
                 } else {
-                    if (offset < array[i+1] - array[i]) {
-                        array[i+1] /= offset;
+                    if (compare(offset, minute(v3, v[i])) == -1) {
                         for(int x=0;x<t;x++) {
-                            array[i+1] *= 10;
+                            v[i+1].push_back(0);
                         }
                     } else {
-                        array[i+1] = array[i] + 1;
+                        v[i+1].clear();
+                        for(int x=0;x<v[i].size();x++) {
+                            v[i+1].push_back(v[i][x]);
+                        }
+
+                        v[i+1][v[i+1].size() - 1] += 1;
                     }
                 }
             }
@@ -85,7 +132,11 @@ int main(int argc, char **argv)
 
 #ifdef FILEIO
         for(int x=0;x<N;x++) {
-            cout << array[x] << " ";
+            for(int y: v[x]) {
+            cout << y;
+            }
+        cout << " ";
+
         }
         cout << endl;
 #endif  
