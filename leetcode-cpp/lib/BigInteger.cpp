c274290d547028e66be4b905c720efe9cc475952
emacslisp <java.gdb@gmail.com>
2021-04-21 23:33:51 +1000

    add BigInteger

---
+++ leetcode-cpp/lib/BigInteger.cpp
@@ -0,0 +1,5 @@
+#include "BigInteger.h"
+
+void BigInteger::MainEntry() {
+    cout << "================= Big Integer =====================" << endl;
+}
---
+++ leetcode-cpp/lib/BigInteger.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
+using namespace std;
+
+class BigInteger {
+public:
+    void MainEntry();
+};
