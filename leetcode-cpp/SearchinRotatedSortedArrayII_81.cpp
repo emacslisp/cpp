9bd6e7a3b86d5059c7be41f2065eae4bebd835af
emacslisp <java.gdb@gmail.com>
2021-05-17 11:55:26 +1000

    finish leetcode 81

---
+++ leetcode-cpp/SearchinRotatedSortedArrayII_81.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
+#include <math.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define fora(i, start, end) for(int i=start;i<end;i++)
+#define forb(i, start, end) for(int i=end;i>=start;i--)
+const double pi=acos(-1.0);
+const double eps=1e-11;
+const int mod = 1e9+7;
+#define mod(n,k) ( ( ((n) % (k)) + (k) ) % (k))
+
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        for(int x : nums) {
+            if(x == target)
+            return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> c
+    {
+       4,5,6,7,0,2,1,3
+    };
+
+    string str = "codeleet";
+    int n = 2;
+    int result = s.search(c, n);
+    cout<<result<<endl;
+}
