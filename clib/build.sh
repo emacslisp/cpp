1bcbbf596ec09cdacc6fe9ae1a872919f9b9b49f
emacslisp <java.gdb@gmail.com>
2021-10-31 23:59:26 +1100

    ignore warning and finish tmpnam

--- clib/build.sh
+++ clib/build.sh
@@ -2,6 +2,6 @@ for fname in *.c
 do
     if [ "${fname%%.*}" != "main" ]; then
         echo "build ${fname%%.*}.c"
-        gcc ${fname%%.*}.c -o ${fname%%.*}.run
+        gcc -w ${fname%%.*}.c -o ${fname%%.*}.run
     fi
 done
